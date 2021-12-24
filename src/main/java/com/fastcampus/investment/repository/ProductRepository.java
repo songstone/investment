a008267c9e2d15225739d337450228c8f9ba924d
Song Seung-hyun <thdtmdgus5505@gmail.com>
2021-12-24 20:52:50 +0900

    feat: implements find method by date
    implements findByStartedAtLessThanAndFinishedAtGreaterThan

--- src/main/java/com/fastcampus/investment/repository/ProductRepository.java
+++ src/main/java/com/fastcampus/investment/repository/ProductRepository.java
@@ -3,5 +3,9 @@ package com.fastcampus.investment.repository;
 import com.fastcampus.investment.entity.Products;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<Products, Long> {
+    List<Products> findByStartedAtLessThanAndFinishedAtGreaterThan(LocalDate curDate1, LocalDate curDate2);
 }
