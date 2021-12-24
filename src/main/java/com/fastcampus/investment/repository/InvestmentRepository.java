f4bb5a044ac4bb881a162252cb52f03460db88d0
Song Seung-hyun <thdtmdgus5505@gmail.com>
2021-12-24 20:50:27 +0900

    feat: implements findByUserId, product method
    implements findByUserIdEqualsOrderByIdAsc
    implements findByUserIdEqualsAndProductEquals

--- src/main/java/com/fastcampus/investment/repository/InvestmentRepository.java
+++ src/main/java/com/fastcampus/investment/repository/InvestmentRepository.java
@@ -1,7 +1,14 @@
 package com.fastcampus.investment.repository;
 
 import com.fastcampus.investment.entity.Investment;
+import com.fastcampus.investment.entity.Products;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface InvestmentRepository extends JpaRepository<Investment, Long> {
+    List<Investment> findByUserIdEqualsOrderByIdAsc(Long userId);
+
+    Optional<Investment> findByUserIdEqualsAndProductEquals(Long userId, Products product);
 }
